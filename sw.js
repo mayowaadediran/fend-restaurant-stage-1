105ed53fdc4c24e91feb82ef1dfd27e245af54d0
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-01-21 23:42:19 +0100

    add files to cache

--- sw.js
+++ sw.js
@@ -1,3 +1,31 @@
+var CACHE_NAME = 'restaurant-review-v1';
+var urlsToCache = [
+  '/',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/index.html',
+  '/restaurant.html'
+];
+
 self.addEventListener('install', function (event) {
-  console.log("installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function (cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
