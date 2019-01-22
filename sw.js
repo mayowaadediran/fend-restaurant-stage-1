e7a5b8f1fcc60dfac78d0f0edd0fb9855eef1f6f
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-01-22 12:41:07 +0100

    fix service worker

--- sw.js
+++ sw.js
@@ -22,10 +22,36 @@ var urlsToCache = [
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(function (cache) {
+    caches.open(CACHE_NAME).then(function (cache) {
       console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
   );
 });
+
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      if(response) {
+        return response;
+      } 
+      return fetch(event.request).then(
+        function(response) {
+          if (!response || response.status !==200 || response.type !== 'basic') {
+            return response;
+          }
+
+          var responseTocache = response.clone();
+
+          caches.open(CACHE_NAME).then(function (cache) {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        }
+      )
+    })
+  );
+});
+
